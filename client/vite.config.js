fbafa8189693f6b8825b7fe8190f51dc5360f5ac
JamLk <jamboy5590@gmail.com>
2023-04-22 23:00:02 -0400

    updated vite base route

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Millogods/viva',
+  base: '/jamlk.github.io',
   plugins: [vue()],
   resolve: {
     alias: {
