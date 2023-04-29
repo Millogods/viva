c50a4d360a96c7fcbda8bf880f53bd985c0bee4a
JamLk <jamboy5590@gmail.com>
2023-04-29 11:17:49 -0400

    Revert "updated vite base route"
    This reverts commit fbafa8189693f6b8825b7fe8190f51dc5360f5ac.

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jamlk.github.io',
+  base: '/Millogods/viva',
   plugins: [vue()],
   resolve: {
     alias: {
